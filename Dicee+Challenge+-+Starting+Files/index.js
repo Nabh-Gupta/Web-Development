0c2a29cc56e1d9c84c67a5e99891c98c477d118a
Nabh Gupta <guptanabh2@gmail.com>
2024-07-28 13:55:12 +0530

    Dice project

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+var randomDiceImage = "dice"+ randomNumber1 +".png";
+var randomImageSource = "images/" + randomDiceImage;
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource)
+
+
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+var randomImageSource2= "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML = "player 1 win";
+    
+} else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML = "player 2 win";
+}
+  else if(randomNumber1 = randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw";
+  }
+
