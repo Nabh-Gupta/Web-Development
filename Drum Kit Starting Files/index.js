b14194aa70be73a93451877544c97f013f38ee14
Nabh Gupta <guptanabh2@gmail.com>
2024-07-29 17:21:20 +0530

    Drum Kit project done

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,34 +1,72 @@
 function CLICKED(){
-    var ButtonHtml = this.innerHTML;
-    switch (ButtonHtml) {
-        case "W":
-            var audio = new Audio("sounds/tom-1.mp3");
-            tom1.play();   
-            break;
-        case "a":
-            var audio = new Audio("sounds/tom-2.mp3");
-            tom2.play(); 
-            break;
-    
-        default:
-   
-            break;
-    }
+    var buttonInnerHtml = this.innerHTML;
+    MakeSound(buttonInnerHtml);
+    ButtonAnimation(buttonInnerHtml);
 }
 
 
 for( i = 0;i<document.querySelectorAll(".drum").length; i++){
  document.querySelectorAll("button")[i].addEventListener("click",CLICKED);{
- 
- 
 }
 }
 
+ document.addEventListener("keydown",function(event) {
+    MakeSound(event.key);
+    ButtonAnimation(event.key);
+ });
+
+
+function MakeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+             var audio = new Audio("sounds/tom-2.mp3");
+             audio.play();
+             break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+            case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+            case "l":
+            var audio = new Audio("sounds/kick-bass.mp3"); 
+            audio.play();
+            break;
+        default:
+          console.log(buttonInnerHtml);
+            break;
+     }
+}
+
+
+function ButtonAnimation(currentKey){
+    var Ani= document.querySelector("."+ currentKey)
+    Ani.classList.add("pressed");
+    setTimeout(function(){
+        Ani.classList.remove("pressed");
+
+    },100);
+}
 
 
 
 
 
 
-// var audio = new Audio("sounds/tom-1.mp3");
-    // audio.play(); 
+
+  //  var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
