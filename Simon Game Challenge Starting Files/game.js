70e21f86a35787270b54b54979df9a3a056b34a7
Nabh Gupta <guptanabh2@gmail.com>
2024-07-31 16:12:02 +0530

    changes

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,19 +1,63 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() *4)
-
-    var randomChosenColour =  buttonColours[randomNumber];
-
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+        playSound(randomChosenColour);
+    }
+
+
+
+$(".btn").on("click", function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+});
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+        audio.play();
+}
+
+function animatePress(currentColor) {
+
+    // 2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+    $("#" + currentColor).addClass("pressed");
+  
+    //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+    
+  setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
 
 
 
 
-    // $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio =new Audio("sounds/"+ randomChosenColour + ".mp3");
-    audio.play();
 
-};
+
+
+
+
+
+
+// function ButtonAnimation(currentKey){
+//     var Ani= document.querySelector("#"+ currentKey)
+//     Ani.classList.add("pressed");
+//     setTimeout(function(){
+//         Ani.classList.remove("pressed");
+//     },1000);
+// }
+
+
+
+
+
+//   $(".btn").click(nextSequence);
